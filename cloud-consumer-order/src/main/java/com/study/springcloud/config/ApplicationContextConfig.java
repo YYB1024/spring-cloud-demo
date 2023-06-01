6db1be4565cf707be1e26cc4b4e48a86e6074509
yangyb <2593819926@qq.com>
2023-06-01 10:16:45 +0800

    使用@LoadBalanced注解赋予RestTemplate负载均衡的能力

--- cloud-consumer-order/src/main/java/com/study/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order/src/main/java/com/study/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.study.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
 
+    // 定义默认的负载均衡机制（使用@LoadBalanced注解赋予RestTemplate负载均衡的能力）
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
