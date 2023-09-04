6d08aedcc31acde7c6b8cb0ffb9bec9292190906
yangyb <2593819926@qq.com>
2023-09-04 10:51:22 +0800

    SpringCloud之GateWay网关-predicates断言

---
+++ cloud-gateway-service/src/test/java/com/study/gateway/GateWayTest.java
@@ -0,0 +1,14 @@
+package com.study.gateway;
+
+import java.time.ZonedDateTime;
+
+/**
+ * @author yangyanbin
+ * @since 20230901
+ **/
+public class GateWayTest {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println("zonedDateTime = " + zonedDateTime);
+    }
+}
