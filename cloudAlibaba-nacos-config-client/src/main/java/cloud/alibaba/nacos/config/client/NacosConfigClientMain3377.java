49dc38dda6a296c6f109a5d8241b702a80753fb4
yangyb <2593819926@qq.com>
2023-10-13 10:43:54 +0800

    SpringCloudAlibaba之Nacos配置中心（三种配合方案）

--- cloudAlibaba-nacos-config-client/src/main/java/cloud/alibaba/nacos/config/client/NacosConfigClientMain3377.java
+++ cloudAlibaba-nacos-config-client/src/main/java/cloud/alibaba/nacos/config/client/NacosConfigClientMain3377.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author yangyanbin
  * @since 20231012
  **/
+// 通过 Spring Cloud 原生注解 @EnableDiscoveryClient 开启服务注册发现功能。
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosConfigClientMain3377 {
